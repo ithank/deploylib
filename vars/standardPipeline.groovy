ffdd011e0acbf02dc048b73596bcb101f4c80653
Steve Elliott <thayland@gmail.com>
2018-04-09 13:05:37 -0500

    Added functionality for groovy scripts

---
+++ vars/standardPipeline.groovy
@@ -0,0 +1,34 @@
+def call(body) {
+
+	// transfer parameters
+	def config = [:]
+	body.resolveStrategy = Closure.DELEGATE_FIRST
+	body.delegate = config
+	body()			
+	
+	node {
+		def app
+		docker.withRegistry('http://10.25.232.183:5000') {
+			stage('Clone') {
+				checkout scm
+			}			
+			stage('Build') {
+			    def buildNumber = getBuildNumber()
+				def buildLabel = getBuildLabel{
+					buildNumber = buildNumber
+				}
+				currentBuild.displayName = "# " + buildLabel
+				app = docker.build(buildLabel)
+			}
+				
+			stage('Test') {
+				app.inside {
+					sh 'echo "Tests passed"'
+				}
+			}
+			stage('Push') { 
+				app.push()
+			}
+		}
+	}
+}
